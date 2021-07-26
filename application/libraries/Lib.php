3a42c4045a5a3e92fa0ac0ad9743051dec9b1ed2
Sarip Hidayat <hidayatsarip2210@gmail.com>
2021-07-26 18:19:29 +0700

    dashboard

--- application/libraries/Lib.php
+++ application/libraries/Lib.php
@@ -19,6 +19,33 @@ class Lib
 		}
     return $hasil;
   }
+	function waktu_lalu($timestamp)
+	{
+		$selisih = time() - strtotime($timestamp) ;
+		$detik = $selisih ;
+		$menit = round($selisih / 60 );
+		$jam = round($selisih / 3600 );
+		$hari = round($selisih / 86400 );
+		$minggu = round($selisih / 604800 );
+		$bulan = round($selisih / 2419200 );
+		$tahun = round($selisih / 29030400 );
+		if ($detik <= 60) {
+				$waktu = $detik.' seconds ago';
+		} else if ($menit <= 60) {
+				$waktu = $menit.' minutes ago';
+		} else if ($jam <= 24) {
+				$waktu = $jam.' hours ago';
+		} else if ($hari <= 7) {
+				$waktu = $hari.' days ago';
+		} else if ($minggu <= 4) {
+				$waktu = $minggu.' weeks ago';
+		} else if ($bulan <= 12) {
+				$waktu = $bulan.' months ago';
+		} else {
+				$waktu = $tahun.' years ago';
+		}
+		return $waktu;
+	}
  
    
 
