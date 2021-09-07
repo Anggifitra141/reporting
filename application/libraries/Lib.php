462016e75b0742bf02df383627746181f77e4626
Anggi Fitrahandika <anggifitra141@gmail.com>
2021-09-07 13:17:21 +0700

    report 304

--- application/libraries/Lib.php
+++ application/libraries/Lib.php
@@ -52,6 +52,31 @@ class Lib
 		}
 		return date('d-m-Y', strtotime($tanggal));
  }
+
+	function date_indonesia($tanggal)
+	{
+		$bulan = array(
+			1 =>   'Januari',
+			'Februari',
+			'Maret',
+			'April',
+			'Mei',
+			'Juni',
+			'Juli',
+			'Agustus',
+			'September',
+			'Oktober',
+			'November',
+			'Desember'
+		);
+		$pecahkan = explode('-', $tanggal);
+
+		// variabel pecahkan 0 = tanggal
+		// variabel pecahkan 1 = bulan
+		// variabel pecahkan 2 = tahun
+
+		return $pecahkan[2] . ' ' . $bulan[(int)$pecahkan[1]] . ' ' . $pecahkan[0];
+	}
  
    
 
