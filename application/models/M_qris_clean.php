969a11fec4c654f16cd1f6b3a4fd6ad3b4a86e26
Anggi Fitrahandika <anggifitra141@gmail.com>
2021-10-01 22:16:32 +0700

    log qris

--- application/models/M_qris_clean.php
+++ application/models/M_qris_clean.php
@@ -164,12 +164,17 @@ class M_qris_clean extends CI_model {
   public function add_qris_merchant($data)
   {
     $this->db->insert($this->table_merchant, $data);
-    return $this->db->insert_id();
+    $id = $this->db->insert_id();
+    user_log($this->session->userdata('id'), 'QRIS MERCHANT', "ADD", $id, "ADD DATA", $this->db->last_query());
+    trx_log($this->session->userdata('id'), 'QRIS MERCHANT', "ADD", $id, "ADD DATA" );
+    return $id;
   }
 
   public function update_qris_merchant($where, $data)
   {
     $this->db->update($this->table_merchant, $data, $where);
+    user_log($this->session->userdata('id'), 'QRIS MERCHANT', "MODIFY", $where['id'], "MODIFY DATA", $this->db->last_query());
+    trx_log($this->session->userdata('id'), 'QRIS MERCHANT', 'MODIFY', $where['id'], "MODIFY DATA");
     return $this->db->affected_rows();
   }
 
@@ -177,6 +182,8 @@ class M_qris_clean extends CI_model {
   {
     $this->db->where('id', $id);
     $this->db->delete($this->table_merchant);
+    user_log($this->session->userdata('id'), 'QRIS MERCHANT', "DELETE", $id, "DELETE DATA", $this->db->last_query());
+    trx_log($this->session->userdata('id'), 'QRIS MERCHANT', "DELETE", $id, "DELETE DATA");
   }
 
 }
